1f33fce52087b871a7f12b32bf2a28cbba7b0deb
lukas.bommelin <lukas.bommelin@elev.ga.ntig.se>
2023-12-11 11:07:23 +0100

    Commit test

---
+++ src/DairyProducts.java
@@ -0,0 +1,5 @@
+public class DairyProducts extends Product {
+    //Constructor
+    //Attributes
+}
+
---
+++ src/Product.java
@@ -0,0 +1,13 @@
+public class Product {
+    //Constructor
+
+    //Attributes
+    protected String name;
+    protected int quantity;
+    protected int price;
+
+    public Product(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+}
